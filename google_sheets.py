7755121c56e57af4c6fe2d386b547a52cd71bce3
Shelby <shelbygt500martin@gmail.com>
2023-06-04 23:33:00 -0400

    probably time to start actually doing commits. Starting to do full refactor, starting with a google sheets class.

--- google_sheets.py
+++ google_sheets.py
@@ -1,12 +1,16 @@
 import gspread
 import json
 import random
+from gspread.cell import Cell
+import xlsxwriter
+import string
+from time import sleep
 
 def open_json(filepath:str):
     with open (filepath) as myfile:
         return json.load(myfile)
     
-def generate_board_layout(board_layout: dict, task_list: dict):
+def generate_board_layout(board_layout:dict, task_list: dict, board_layout_filename:str="board_layout.json"):
     generated_layout = {}
     used_tasks = []
     for task_difficulty in board_layout:
@@ -18,8 +22,15 @@ def generate_board_layout(board_layout: dict, task_list: dict):
                     used_tasks.append(random_task)
                     task = random_task
             generated_layout[tile]=task
-    with open("new_board_layout.json", "w") as myfile:
+    with open(board_layout_filename, "w") as myfile:
         json.dump(generated_layout, myfile)
+
+def col2num(col):
+    num = 0
+    for c in col:
+        if c in string.ascii_letters:
+            num = num * 26 + (ord(c.upper()) - ord('A')) + 1
+    return num
 
 class google_sheets:
     def __init__(self, google_credentials_filepath:str, spreadsheet_name:str):
@@ -28,23 +39,132 @@ class google_sheets:
         self.current_sheet = self.worksheet.get_worksheet(0)
 
     def change_to_sheet(self, sheet_name:str):
-        self.current_sheet = self.worksheet(sheet_name)
+        self.current_sheet = self.worksheet.worksheet(sheet_name)
 
-    def create_scoreboard_sheet(self, board_layout:dict):
-        try:
-            self.change_to_sheet("Scoreboard2")
-        except TypeError:
-            self.current_sheet = self.worksheet.add_worksheet(title="Scoreboard2", rows=100, cols=20)
-        #need to include formatting to make it look pretty going forwards.
-        
+    def create_scoreboard(self):
+        if self.current_sheet.title != "Scoreboard":
+            try:
+                self.change_to_sheet("Scoreboard")
+            except gspread.exceptions.WorksheetNotFound:
+                self.current_sheet = self.worksheet.add_worksheet(title="Scoreboard", rows=100, cols=20)
 
+    def format_scoreboard(self, board_template:dict, board_layout:dict):
+        cell_formats = []
+        cell_formats.append(
+            #bolden member names
+            {
+                "range": "c1:",
+                "format": {
+                    "textFormat": {
+                        "bold": True
+                    },
+                },
+            }
+        )
+        for cell in board_layout:
+            if board_layout[cell]=="START":
+                cell_formats.append(
+                    {
+                        "range": cell,
+                        "format": {
+                            "backgroundColor": {
+                                "red": 0.0,
+                                "green": 1.0,
+                                "blue": 0.0
+                            },
+                            "borders": {
+                                "top": {
+                                    "style": "SOLID",
+                                },
+                                "bottom": {
+                                    "style": "SOLID",
+                                },
+                                "left": {
+                                    "style": "SOLID",
+                                },
+                                "right": {
+                                    "style": "SOLID",
+                                }
+                            },
+                            "textFormat": {
+                                "bold": True,
+                                "fontSize": 20
+                            },
+                            "horizontalAlignment": "CENTER",
+                            "verticalAlignment": "MIDDLE",
+                            "wrapStrategy": "WRAP"
+                        }
+                    }
+                )
+            else:
+                cell_formats.append(
+                    {
+                        "range": cell,
+                        "format": {
+                            "borders": {
+                                "top": {
+                                    "style": "SOLID",
+                                },
+                                "bottom": {
+                                    "style": "SOLID",
+                                },
+                                "left": {
+                                    "style": "SOLID",
+                                },
+                                "right": {
+                                    "style": "SOLID",
+                                }
+                            },
+                            "horizontalAlignment": "CENTER",
+                            "verticalAlignment": "MIDDLE",
+                            "wrapStrategy": "WRAP"
+                        }
+                    }
+                )
+        self.current_sheet.batch_format(cell_formats)
+        pass
 
-
+    def write_to_scoreboard(self, board_layout:dict, individual_tile:str=None):
+        if self.current_sheet.title != "Scoreboard":
+            try:
+                self.change_to_sheet("Scoreboard")
+            except gspread.exceptions.WorksheetNotFound:
+                print("No scoreboard located to write value to.")
+                return False
+        cells = []
+        tiles_to_update = []
+        if individual_tile==None:
+            tiles_to_update = [tile for tile in board_layout]
+        elif individual_tile in [tile for tile in board_layout]:
+            tiles_to_update.append(individual_tile)
+        else:
+            print("Inavlid tile selected")
+            return False
+        for tile in tiles_to_update:
+            row = []
+            column = []
+            for letter in tile:
+                if letter.isnumeric():
+                    row.append(letter)
+                else:
+                    column.append(letter)
+            row = int("".join(row))
+            column = col2num("".join(column))
+            cells.append(Cell(row, column, board_layout[tile]))
+        self.current_sheet.update_cells(cells)
+        print("Successfully updated tiles.")
+        return True
 
 
 if __name__ == "__main__":
-    board_layout = open_json("./config/board_layout.json")
+    
+    board_template = open_json("./config/board_template.json")
     tasks = open_json("./config/tasks.json")
-    generate_board_layout(board_layout, tasks)
+    generate_board_layout(board_template, tasks)
+    board_layout = open_json("./board_layout.json")
     team_1_sheets = google_sheets("./credentials.json", "Team 1")
-    team_1_sheets.create_scoreboard_sheet()
+    team_1_sheets.create_scoreboard()
+    #team_1_sheets.write_to_scoreboard(board_layout, "f7")
+    # team_1_sheets.write_to_scoreboard(board_layout, "f10")
+    team_1_sheets.format_scoreboard(board_template,board_layout)
+    team_1_sheets.write_to_scoreboard(board_layout)
